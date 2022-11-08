02b4770c9ec6ee989db76c91b2c37a380c61f5ff
ElmarFazlagic93 <elmar.fazlagic@gmail.com>
2022-11-08 23:04:55 +0100

    Initial Commit

---
+++ MoviesApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MoviesApp
+//
+//  Created by Elmar Fazlagic on 8. 11. 2022..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MoviesApp/MoviesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesAppApp.swift
+//  MoviesApp
+//
+//  Created by Elmar Fazlagic on 8. 11. 2022..
+//
+
+import SwiftUI
+
+@main
+struct MoviesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
